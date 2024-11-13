40fe03263a8178e9ba5eb6845b7aa3069f471354
Tortoise247 <132410700+Tortoise247@users.noreply.github.com>
2024-11-14 00:16:18 +0900

    Read Xml
    export json

--- JSON_XML.ps1
+++ JSON_XML.ps1
@@ -15,3 +15,26 @@ $xml = $array | ConvertTo-Xml -As String
 
 # Output the XML
 $xml
+
+# sample.xml ファイルを読み込む
+$xmlPath = "sample.xml"
+$xmlData = [xml](Get-Content -Path $xmlPath)
+
+# すべての author 要素を取得する
+$authorElements = $xmlData.SelectNodes("//author")
+
+# 各 author 要素のテキストを取得する
+$authors = @()
+foreach ($author in $authorElements) {
+    $authors += $author.InnerText
+}
+
+# author 要素のテキストを表示する
+$authors
+
+
+# author 要素のテキストを JSON に変換する
+$jsonData = $authors | ConvertTo-Json -Depth 10
+
+# JSON データを表示する
+$jsonData
